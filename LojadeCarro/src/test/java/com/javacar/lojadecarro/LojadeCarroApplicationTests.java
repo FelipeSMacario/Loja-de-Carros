fa531c08c6178b7f94a0d033d4a70207a719688e
Felipe Macario <felipesmacario@gmail.com>
2026-07-08 14:32:41 -0300

    implementando github actions

--- LojadeCarro/src/test/java/com/javacar/lojadecarro/LojadeCarroApplicationTests.java
+++ LojadeCarro/src/test/java/com/javacar/lojadecarro/LojadeCarroApplicationTests.java
@@ -2,12 +2,14 @@ package com.javacar.lojadecarro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class LojadeCarroApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
