5c442d1f9bb52da1956a4f3fbdb3d9e18b0996c0
Felipe Macario <felipesmacario@gmail.com>
2026-07-19 00:18:54 -0300

    Finalização da refatoração do projeto

--- LojadeCarro/src/main/resources/db/migration/V2__insert_status_veiculo.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO status_veiculo (nome) VALUES
-                                      ('DISPONIVEL'),
-                                      ('RESERVADO'),
-                                      ('VENDIDO'),
-                                      ('PAUSADO');
